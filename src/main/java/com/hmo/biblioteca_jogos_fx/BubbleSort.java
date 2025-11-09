a9c7d3afa1700c668a73e272885dd06a450bfd5f
ViktorSchmidtTrawinski <vi.trawinski@gmail.com>
2025-11-09 11:58:16 -0300

    Lógica Bubble Sort com recursividade sem a implementação no FX

--- src/main/java/com/hmo/biblioteca_jogos_fx/BubbleSort.java
+++ src/main/java/com/hmo/biblioteca_jogos_fx/BubbleSort.java
@@ -3,29 +3,28 @@ package com.hmo.biblioteca_jogos_fx;
 import java.util.List;
 
 public class BubbleSort {
-
-    public static void ordenarPorTitulo(List<Jogo> jogos) {
-        int n = jogos.size();
-        boolean trocou;
-
-        for (int i = 0; i < n - 1; i++) {
-            trocou = false;
-            for (int j = 0; j < n - i - 1; j++) {
-                String tituloAtual = jogos.get(j).getTitulo();
-                String tituloProximo = jogos.get(j + 1).getTitulo();
-
-                if (tituloAtual.compareToIgnoreCase(tituloProximo) > 0) {
-                    // Troca os elementos de posição caso estejam fora de ordem
-                    Jogo temp = jogos.get(j);
-                    jogos.set(j, jogos.get(j + 1));
-                    jogos.set(j + 1, temp);
-                    trocou = true; // Indica que houve troca
-                }
-            }
-
-            // Se nenhuma troca ocorreu ele sai do loop
-            if (!trocou) break;
+    public static void ordenarPorTitulo(List<Jogo> jogos, int n) {
+        // se n == 1 a lista ordenada
+        if (n == 1) {
+            return;
         }
+        for (int i = 0; i < n - 1; i++) {
+            String tituloAtual = jogos.get(i).getTitulo();
+            String tituloProximo = jogos.get(i + 1).getTitulo();
+            if (tituloAtual.compareToIgnoreCase(tituloProximo) > 0) {
+                // Troca os elementos se estiverem fora de ordem
+                Jogo temp = jogos.get(i);
+                jogos.set(i, jogos.get(i + 1));
+                jogos.set(i + 1, temp);
+            }
+        }
+        // Chamada  para ordenar  primeiros elementos
+        ordenarPorTitulo(jogos, n - 1);
+    }
+    //metodo auxiliar sem inserção do n manualmente
+    public static void ordenarPorTitulo(List<Jogo> jogos) {
+        ordenarPorTitulo(jogos, jogos.size());
     }
 }
+
 
